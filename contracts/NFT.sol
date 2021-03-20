1bffe5c87040e88a5a5012c8b074dc22d363bfa4
chrischakrit <chakrit_th@hotmail.com>
2021-03-20 00:42:52 -0400

    Initial commit for smart contracts

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,15 +3,16 @@ pragma solidity >=0.6.0 <0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
 
 contract NFT is ERC721, Ownable {
     address payable public _owner;
-    mapping(uint256 => bool) public sold;
+    mapping(uint256 => bool) public minted;
     mapping(uint256 => uint256) public price;
 
-    event Purchase(address owner, uint256 price, uint256 id, string uri);
+    event Mint(address owner, uint256 price, uint256 id, string uri);
 
-    constructor() ERC721("Decentreelized Token", "DTT") {
+    constructor() ERC721("Decentreelized Token", "TRE") {
         _owner = msg.sender;
     }
 
@@ -29,22 +30,22 @@ contract NFT is ERC721, Ownable {
         return true;
     }
 
-    function buy(uint256 _id) external payable {
+    function donate(uint256 _id) external payable {
         _validate(_id); //check req. for trade
         _trade(_id); //swap nft for eth
 
-        emit Purchase(msg.sender, price[_id], _id, tokenURI(_id));
+        emit Mint(msg.sender, price[_id], _id, tokenURI(_id));
     }
 
     function _validate(uint256 _id) internal {
         require(_exists(_id), "Error, wrong Token id"); //not exists
-        require(!sold[_id], "Error, Token is sold"); //already sold
+        require(!minted[_id], "Error, Token is minted"); //already minted
         require(msg.value >= price[_id], "Error, Token costs more"); //costs more
     }
 
     function _trade(uint256 _id) internal {
         _transfer(address(this), msg.sender, _id); //nft to user
         _owner.transfer(msg.value); //eth to owner
-        sold[_id] = true; //nft is sold
+        minted[_id] = true; //nft is minted
     }
 }
